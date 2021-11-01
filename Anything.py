62fa1a43c132bf34e8efddc862e58f0b01a2b49e
quioxe <sebacazan@gmail.com>
2021-11-01 21:42:22 +0100

    commit

--- Anything.py
+++ Anything.py
@@ -1,24 +1,26 @@
-#Les vertébrés------------------------------------------------
-oiseaux = ["perroquet"]
-amphibiens = []
-poissons = []
-reptiles = []
-mammifère = ["humain", "ours", "loup", "chien", "renard", "dauphin", "lapin", "chat"]
-chordés = [mammifère, reptiles, poissons, amphibiens, oiseaux]
-#-------------------------------------------------------------
-vertébrés = [chordés]
+#Les vertébrés-------------------------------------------------------------------------------
+oiseau = ["perroquet", "poule", "poulet", "coq", "canard", "dinde", "dindon", "colibri", "mésange", "rouge-gorge", "corbeau", "perruche", "canard"]
+amphibien = ["grenouilles", "crapauds", "salamandres"]
+poisson = ["anguille", "saumon", "sardines", "maquereau", "morue", "thon", "raie", "gobie", "poisson rouge", "poisson clown"]
+reptile = ["crocodiles", "serpents", "alligators", "lézards", "tortue"]
+mammifère = ["humain", "ours", "loup", "chien", "renard", "dauphin", "lapin", "chat", "chaton", "chauve-souris", "balleine", "hérisson", "hamster", "cochon d'inde"]
+chordé = [mammifère, reptile, poisson, amphibien, oiseau]
+#--------------------------------------------------------------------------------------------
+vertébré = [chordé]
 
-#Les invertébrés---------------
-arachnides = ["araignées"]
-crustacés = ["crabe", "homard", "langouste", "crevette"]
-insectes = ["moustiques", "libellules", "papillons"]
-arthropodes = [insectes, crustacés, arachnides]
-mollusques = ["escargot"]
-#------------------------------
-invertébrés = [mollusques, arthropodes]
+#Les invertébrés-----------------------------------------------------------------------------
+arachnide = ["araignées"]
+crustacé = ["crabe", "homard", "langouste", "crevette"]
+insecte = ["moustiques", "libellules", "papillons"]
+arthropode = [insecte, crustacé, arachnide]
+mollusque = ["escargot", "moule", "huitre", "saint-jacques", "palourde", "bigorneau", "pétoncle", "amande de mer", "bucarde", "bulot", "clam", "coque", "ormeau", "patelle", "pouce-pied", "praire", "telline"]
+#--------------------------------------------------------------------------------------------
+invertébré = [mollusque, arthropode]
 
 #Les ANIMAUX
-animaux = [vertébrés, invertébrés]
+animal = [vertébré, invertébré]
 
 #La liste FINALE
-elements = [animaux]
+elements = [animal]
+
+inp = input("Quel élément voulez-vous trier : ")
