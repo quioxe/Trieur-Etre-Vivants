567898176e18c3b921b2a0b5e233c8e149e0d40f
quioxe <sebacazan@gmail.com>
2021-11-01 13:07:11 +0100

    First commit

---
+++ Anything.py
@@ -0,0 +1,24 @@
+#Les vertébrés-----------------
+oiseaux = []
+amphibiens = []
+poissons = []
+reptiles = []
+mammifère = []
+chordés = [mammifère, reptiles, poissons, amphibiens, oiseaux]
+#------------------------------
+vertébrés = [chordés]
+
+#Les invertébrés---------------
+arachnides = []
+crustacés = []
+insectes = []
+arthropodes = [insectes, crustacés, arachnides]
+mollusques = []
+#------------------------------
+invertébrés = [mollusques, arthropodes]
+
+#Les ANIMAUX
+animaux = [vertébrés, invertébrés]
+
+#La liste FINALE
+elements = [animaux]
---
+++ README.md
@@ -0,0 +1,1 @@
+# Trieur-Etre-Vivants
